74586cc0b63a94daa200723e8fcebdbd13a55bf8
z-robin.ekerhag <robin.ekerhag.external@veoneer.com>
2019-03-07 10:54:52 +0100

    Update files to enable rspec-tests
    Add rspec tests for leaving optional parameters empty

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,10 @@ default_fact_files.each do |f|
     RSpec.configuration.reporter.message "WARNING: Unable to load #{f}: #{e}"
   end
 end
+RSpec.configure do |c|
+  c.mock_with :rspec
+end
+require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
   c.default_facts = default_facts
